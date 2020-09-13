5f608b55f6272500c6e4d070a7d8dbc1de67d37f
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2020-09-13 13:21:13 -0500

    Added cmake files

---
+++ CMakeFiles/sockChat.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/sockChat.dir/src/server.cpp.o"
+  "sockChat"
+  "sockChat.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/sockChat.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
