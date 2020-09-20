3cc339c0a99a39c2a740f72b5485f21896bf6e36
Astrol99 <41875791+Astrol99@users.noreply.github.com>
2020-09-20 18:46:30 -0500

    Basic backbone for listener/server

--- src/server.cpp
+++ src/server.cpp
@@ -2,8 +2,26 @@
 #include <unistd.h>
 #include <netdb.h>
 
-void initServer(int *PORT) {
-    //int serverFd = socket(AF_INET, SOCK_STREAM, 0);
+void initServer(int PORT) {
+    int serverFd, conn;
+    struct sockaddr_in addr;
+
+    serverFd = socket(AF_INET, SOCK_STREAM, 0);
+
+    // Binding socket to port
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(PORT);
+
+    bind(serverFd, (struct sockaddr *)&addr, sizeof(addr));
+
+    listen(serverFd, 3);
+
+    conn = accept(serverFd, (struct sockaddr *)&addr, (socklen_t*)sizeof(addr));
+
+    char buffer[1024] = {0};
+    read(conn, buffer, 1024);
+    std::cout << buffer << std::endl;
 }
 
 int main(int argc, char **argv) {
@@ -14,7 +32,7 @@ int main(int argc, char **argv) {
     }
     int PORT = std::stoi(argv[1]);
 
-    initServer(&PORT);
+    initServer(PORT);
 
     return 0;
 }
